582552f6b9e1ab7992a8d7ebe93d01eb671e97d5
Fredrickmureti <fredrickmureti612@gmail.com>
2024-07-03 23:08:11 +0300

    did minor edit

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://motorcycle-sales-api.vercel.app';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://motorcycle-sales-api.vercel.app';
 
 // Fetch all motorcycles
 export const fetchMotorcycles = async () => {
